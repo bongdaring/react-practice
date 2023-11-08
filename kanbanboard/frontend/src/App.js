5a51de48928576e41ce66dadc33821ec8c77fc7f
bongdaring <chlwlsdud258@gmail.com>
2023-11-08 11:11:49 +0900

    kanbanboard#5

--- kanbanboard/frontend/src/App.js
+++ kanbanboard/frontend/src/App.js
@@ -1,11 +1,15 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import './assets/css/App.css';
 import KanbanBoard from './KanbanBoard';
 
 function App() {
+
+
+
     return (
         <div id={'App'} className={'App'}>
             <KanbanBoard />
+            
         </div>
     );
 }
